7f8a13e3375f3496a4cc7d2f68e0f7f46375a52a
sadia00-dev <umesadia757@gmail.com>
2026-06-23 15:55:25 +0500

    fix file rename issue

--- script.js
+++ script.js
@@ -4,41 +4,8 @@ const pass = document.getElementById("pass");
 
 const userError = document.getElementById("userError");
 const passError = document.getElementById("passError");
-
-const nameRegex = /^[A-Za-z\s]+$/;
-
-// Username Validation While Typing
-username.addEventListener("input", function () {
-  if (!nameRegex.test(username.value)) {
-    userError.textContent = "Numbers are not allowed.";
-    username.classList.add("error-border");
-    username.classList.remove("success-border");
-  } else {
-    userError.textContent = "";
-    username.classList.remove("error-border");
-    username.classList.add("success-border");
-  }
-});
-
-// Password Validation While Typing
-pass.addEventListener("input", function () {
-  if (pass.value.length < 8) {
-    passError.textContent = "Password must be at least 8 characters long";
-
-    pass.classList.add("error-border");
-    pass.classList.remove("success-border");
-  } else if (!/[A-Z]/.test(pass.value)) {
-    passError.textContent = "1 Capital Letter must be included";
-
-    pass.classList.add("error-border");
-    pass.classList.remove("success-border");
-  } else {
-    passError.textContent = "";
-
-    pass.classList.remove("error-border");
-    pass.classList.add("success-border");
-  }
-});
+const age = document.getElementById("above 18");
+const age = document.getElementById("under 18");
 
 // Form Submit Validation
 form.addEventListener("submit", function (e) {
